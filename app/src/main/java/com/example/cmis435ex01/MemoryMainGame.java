3e31dad0a26039573cf8f1fd7d72d677e3941c53
dasMichal <michalxd@live.de>
2021-04-04 02:24:13 +0200

    Added Game Logic
    Exposing the Recyclerview elements to the MemoryMainGame.java

--- app/src/main/java/com/example/cmis435ex01/MemoryMainGame.java
+++ app/src/main/java/com/example/cmis435ex01/MemoryMainGame.java
@@ -1,6 +1,11 @@
 package com.example.cmis435ex01;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
@@ -28,7 +33,14 @@ public class MemoryMainGame extends AppCompatActivity
 	private int secondPos;
 	private String firstTag;
 	private String secondTag;
+	TextView firstText;
+	TextView secondText;
+	CardView firstCard;
+	CardView secondCard;
 
+	static final int duration = 800;
+	private final int score = 0;
+	public CountDownTimer counter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -52,14 +64,15 @@ public class MemoryMainGame extends AppCompatActivity
 			}
 
 
-			public void test(CardView v, int itemCount, int layoutPosition, RecycleAdapter.ViewHolder viewHolder)
+			public void test(CardView v, TextView text, int itemCount, int layoutPosition, RecycleAdapter.ViewHolder viewHolder)
 			{
 				//System.out.println(v);
-				System.out.println("itemCount = " + itemCount);
-				System.out.println("layoutPosition = " + layoutPosition);
+				//System.out.println("itemCount = " + itemCount);
+				//System.out.println("layoutPosition = " + layoutPosition);
 				//recyclerView.getChildAt(layoutPosition).setEnabled(false);
 
-				logic(v,itemCount, layoutPosition, viewHolder);
+				Log.d("MainGame","itemCount = " + itemCount);
+				logic(v,text,itemCount, layoutPosition, viewHolder);
 
 			}
 
@@ -74,30 +87,31 @@ public class MemoryMainGame extends AppCompatActivity
 		generateData();
 		Collections.shuffle(memCardContent);
 
-
 	}
 
-	private void logic(CardView v, int itemCount, int layoutPosition, RecycleAdapter.ViewHolder viewHolder)
+	private void logic(CardView v,TextView text, int itemCount, int layoutPosition, RecycleAdapter.ViewHolder viewHolder)
 	{
-
 
 
 		if (countFlipped == 0)
 		{
-			System.out.println("First flip");
+
+			Log.d("GameLogic","First Card Flip");
 			firstPos = layoutPosition;
 			firstTag = (String) v.getTag();
+			firstText = text;
+			firstCard = v;
 			countFlipped++;
 
 
 		}else if (countFlipped == 1)
 		{
-			System.out.println("Second flip");
+			Log.d("GameLogic","Second Card Flip");
 			secondPos = layoutPosition;
 			secondTag = (String) v.getTag();
-
+			secondText = text;
+			secondCard = v;
 			countFlipped++;
-
 		}
 
 
@@ -105,37 +119,87 @@ public class MemoryMainGame extends AppCompatActivity
 		if(countFlipped == 2)
 		{
 
-			System.out.println("compare(firstTag,secondTag) = " + compare(firstTag, secondTag));
+
+			Log.d("Game Logic","compare(firstTag,secondTag) = " + compare(firstTag, secondTag));
+
+
+
+
 
 			if (compare(firstTag,secondTag))
 			{
 
 				recyclerView.getChildAt(firstPos).setEnabled(false);
 				recyclerView.getChildAt(secondPos).setEnabled(false);
+
+				firstCard.setCardBackgroundColor(getColor(R.color.md_green_400));
+				secondCard.setCardBackgroundColor(getColor(R.color.md_green_400));
+
+
+
+			}else
+			{
+
+				TypedArray a = getTheme().obtainStyledAttributes(R.style.Theme_CMIS435Ex01, new int[] { R.attr.cardBackgroundColor });
+
+				// Get color hex code (eg, #fff)
+				int intColor = a.getColor(0 /* index */, 0 /* defaultVal */);
+				String hexColor = Integer.toHexString(intColor);
+				// Don't forget to recycle
+				a.recycle();
+
+
+				firstCard.setCardBackgroundColor(getColor(R.color.md_red_200));
+				secondCard.setCardBackgroundColor(getColor(R.color.md_red_200));
+
+				progress3(firstText,secondText,firstPos,secondPos, intColor); // Creating a Countdown timer
+				counter.cancel(); // For Safety cancel a possible running countdown Timer
+				counter.start(); // Start a new countdown Timer
+
+
 			}
-
-
 
 			countFlipped = 0;
 		}
 
+	}
 
 
 
+	private void progress3(TextView firstText, TextView secondText, int firstPos, int secondPos, int defaultColour )
+	{
+
+		counter = new CountDownTimer(duration, 1000)
+		{
+			final int half = ((duration / 2) / 1000);
+			final int progressStatus = 0;
+
+			@Override
+			public void onTick(long millisUntilFinished)
+			{
+
+
+			}
+
+			@Override
+			public void onFinish()
+			{
+
+
+				firstText.setVisibility(View.INVISIBLE);
+				secondText.setVisibility(View.INVISIBLE);
+				firstCard.setCardBackgroundColor(defaultColour);
+				secondCard.setCardBackgroundColor(defaultColour);
+
+			}
+		};
 	}
 
 
 	private boolean compare(String tag1,String tag2)
 	{
-
 		return tag1.equalsIgnoreCase(tag2);
-
-
 	}
-
-
-
-
 
 
 	private void generateData()
@@ -151,10 +215,6 @@ public class MemoryMainGame extends AppCompatActivity
 		adapter.notifyDataSetChanged();
 
 	}
-
-
-
-
 
 
 
